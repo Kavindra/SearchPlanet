01a9a5325af0623f942ee38ed38452efa919bf57
vasana <jayachamaree@gmail.com>
2013-08-03 04:07:08 +0530

    updated with formatting and datastore

--- src/com/planet/SearchPlanetServlet.java
+++ src/com/planet/SearchPlanetServlet.java
@@ -3,13 +3,18 @@ package com.planet;
 import java.io.IOException;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.*;
 
 import com.google.gson.Gson;
+import com.googlecode.objectify.Key;
 import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.annotation.Parent;
 
 @SuppressWarnings("serial")
 public class SearchPlanetServlet extends HttpServlet {
@@ -22,8 +27,12 @@ public class SearchPlanetServlet extends HttpServlet {
 		//resp.getWriter().println("Hello, world");
 	}
 	
+	@Parent Key<SearchPlanet> parentKey;
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		
+		try{
+			
 		
 		//String action = req.getParameter("action");
 		String planetName = req.getParameter("planet");
@@ -38,9 +47,17 @@ public class SearchPlanetServlet extends HttpServlet {
 			getCentersForDealer(req,result);
 		}*/
 
+		
+		//DatabaseFactory dbFactory = new DatabaseFactory();
+		//List<SearchPlanet> planetList = dbFactory.getStoredEntries();
+		
 		//Add it back to session
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("application/json; charset=UTF-8");
+		
+		}catch(Exception e){
+			System.out.println("Servlet error:"+e);
+		}
 		//resp.getWriter().print(new Gson().toJson(result));
 	}
 	
@@ -71,10 +88,12 @@ public class SearchPlanetServlet extends HttpServlet {
 			
 			save(planet);
 			
-			resp.setStatus(0);
+			resp.setStatus(1);
+						
 		}catch(Exception e){
 			e.printStackTrace();
 			resp.setStatus(-1);
+			System.out.println("Error");
 		}
 		
 		
@@ -82,6 +101,37 @@ public class SearchPlanetServlet extends HttpServlet {
 	}
 	
 	public void save(SearchPlanet sp){
+		
+		List<SearchPlanet> planetList = db.query(SearchPlanet.class).list();
+
+		int count = planetList.size();
+		
+		if (count >= 10){
+			
+			List<Long> sortList = new ArrayList<Long>();
+			for (SearchPlanet planet: planetList){
+				sortList.add(planet.id);
+			}
+			while(sortList.size() >= 10){
+				SearchPlanet firstEntry = db.get(new Key<SearchPlanet>(SearchPlanet.class, sortList.get(0)));
+				db.delete(firstEntry);	
+				sortList.remove(sortList.get(0));
+			}
+			
+		}
+		
 		db.put(sp);
+		
+		planetList.clear();
+		planetList = db.query(SearchPlanet.class).list();
+		
+		System.out.println("Current Planet Search List:");
+		
+		for (SearchPlanet planet: planetList){
+			System.out.println(planet.searchWord);
+		}
+		
+		
+	
 	}
 }
